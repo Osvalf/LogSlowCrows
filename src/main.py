c67db54fc16a28ad6120c520499c70d876326615
Brizeh <alexislamusique@gmail.com>
2024-01-03 16:47:01 +0100

    Ajout de tests

--- src/main.py
+++ src/main.py
@@ -9,14 +9,22 @@ if __name__ == "__main__":
 
     print("\n",boss.current.mvp,"\n") # Get le mvp
     
+    # Exemple affichage des rôle
+    alacs,quicks,dpss=[],[],[]   
     for i in range(10):
         player_name = boss.current.get_player_name(i)
         if(boss.current.is_alac(i)):
-            print(f"{player_name} est ALAC")
-        elif(boss.current.is_quick(i)):
-            print(f"{player_name} est QUICK")
+            alacs.append(boss.current.get_player_name(i))
+        if(boss.current.is_quick(i)):
+            quicks.append(boss.current.get_player_name(i))
         else:
-            print(f"{player_name} est DPS")
+            dpss.append(boss.current.get_player_name(i))
+    for e in alacs:
+        print(f"{e} est ALAC")
+    for e in quicks:
+        print(f"{e} est QUICK")
+    for e in dpss:
+        print(f"{e} est DPS")
 
     print(f"\nListe de tous les boss instanciés : {all_bosses}\n") # Afficher toutes les instances
     
